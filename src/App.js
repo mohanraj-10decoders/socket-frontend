23bf9a3060068b2ead60653cc064a4c1fe0abcbd
mohanraj-10decoders <mohanraj.l@10decoders.in>
2022-07-18 16:18:37 +0530

    Basic functionalities

--- src/App.js
+++ src/App.js
@@ -1,22 +1,51 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import { io } from 'socket.io-client';
+import { nanoid } from 'nanoid';
 import './App.css';
 
+const socket = io('http://localhost:5000');
+const userName = nanoid(10);
+
 function App() {
+  const [message, setMessage] = useState('');
+  const [chat, setChat] = useState([]);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    socket.emit('chat', { message, userName });
+    setMessage('');
+  };
+
+  useEffect(() => {
+    socket.on('chat', (payload) => {
+      setChat([...chat, payload]);
+    });
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Chat App</h1>
+        {chat.map((payload, index) => {
+          return (
+            <p key={index} className='msgData'>
+              <section>{payload.message}</section>
+              <span> id:{payload.userName}</span>
+            </p>
+          );
+        })}
+        <form onSubmit={sendMessage}>
+          <input
+            type='text'
+            name='chat'
+            placeholder='send text'
+            value={message}
+            onChange={(evt) => {
+              setMessage(evt.target.value);
+            }}
+          />
+          <button type='submit'>Send</button>
+        </form>
       </header>
     </div>
   );
